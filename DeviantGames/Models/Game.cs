33d6311604e9acb6c5f1b8187f8233787acc5380
dev-marko <markospasenovski00@gmail.com>
2021-11-26 00:40:33 +0100

    Forgot to add no-args constructor to Game class

--- DeviantGames/Models/Game.cs
+++ DeviantGames/Models/Game.cs
@@ -13,9 +13,15 @@ namespace DeviantGames.Models
         public string Publisher { get; set; }
         public List<Genre> Genres { get; set; }  // ponatamu smeni go ova vo enumeracija 
         public string CoverImage { get; set; }
-        public List<string> ExtraImages { get; set; }
+        public List<string> ExtraImages { get; set; }   // URLs
         public decimal Price { get; set; }  // rabotime samo so euraaa
         public string Description { get; set; }
         public DateTime ReleaseDate { get; set; }
+
+        public Game()
+        {
+            this.Genres = new List<Genre>();
+            this.ExtraImages = new List<string>();
+        }
     }
 }
